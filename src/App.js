76ad3d7ff78d5a186a5bb21fd9fb04381e8f52dd
ketsebaot-ertumo <ertumoketsebaot@gmail.com>
2025-04-22 19:47:55 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Thanks from './pages/Thanks';
-  
-    
-  
-  
   
   
 const App = () => {
     return (
       <>
           <ToastContainer />
-              <BrowserRouter>
+              {/* <BrowserRouter> */}
+              <HashRouter>
                 <Routes>
                   <Route path='/' element={<Home />} />
                   <Route path='/thank-you' element={<Thanks />} />
                 </Routes>
-              </BrowserRouter>
+              </HashRouter>
+              {/* </BrowserRouter> */}
           
       </>
     );
